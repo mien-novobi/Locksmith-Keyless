657d82b5f2c2c533b0c13c3c47f627ecbb031531
Vishnu M <vishnu.m@confianzit.biz>
2021-04-01 15:13:34 +0530

    [IMP] CHANNEL_ADVISOR: cleanup - Creating and Validating Invoice

--- channel_advisor/models/edi_log.py
+++ channel_advisor/models/edi_log.py
@@ -329,9 +329,6 @@ class TransactionLogger(models.Model):
                             pack.write({'qty_done': pack.product_qty})
                     saleorder.picking_ids.action_done()
 
-            # Creating and Validating Invoice
-            invoices = saleorder._create_invoices(final=True)
-            invoices.post()
         return saleorder
 
 
@@ -358,6 +355,12 @@ class TransactionLogger(models.Model):
                 try:
                     SaleOrder = self.create_order(vals,center_dict)
                     cr.commit()
+
+                    # Creating and Validating Invoice
+                    if SaleOrder.invoice_status == 'to invoice':
+                        invoices = SaleOrder._create_invoices(final=True)
+                        invoices.post()
+                        cr.commit()
                 except Exception as e:
                     cr.rollback()
                     error_message = e
