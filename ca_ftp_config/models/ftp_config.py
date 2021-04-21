2e41d4ce6547b536a8e56a94184fe6149958946b
Vishnu M <vishnu.m@confianzit.biz>
2021-04-21 12:51:13 +0530

    [IMP] CA_FTP_CONFIG: Moving processed files another directory

--- ca_ftp_config/models/ftp_config.py
+++ ca_ftp_config/models/ftp_config.py
@@ -45,28 +45,21 @@ class FtpConfig(models.Model):
         else:
             raise UserError(_('Login Unsuccessful! May Be Incorrect Settings!'))
 
+    def process_ftp_file(self):
+        cr = self.env.cr
+        SaleOrder = self.env['sale.order'].sudo()
+        ftp_configs = self.search([])
 
-    def process_ftp_file(self):  # , automatic=False, use_new_cursor=False
-        try:
-            context = dict(self._context or {})
-            FtpConfig = self.env['ftp.configuration'].search([])#.browse([1])#
-            if not FtpConfig:
-                return False
-            for config in FtpConfig:
+        for config in ftp_configs:
+            if config.protocol == 'ftp':
+                ftp = False
                 try:
-                    files = []
-                    if  config.protocol == 'sftp':
-                        pass
-                    elif config.protocol == 'ftp'  :
-                        ftp = FTP(config.host)
-                        login_status = ftp.login(config.username, config.password)
-                        ftp.cwd(config.dest_file_location)
-                        processed_file_list = ftp.nlst()
-                        ftp.cwd('/')
+                    ftp = FTP(config.host)
+                    if ftp.login(config.username, config.password):
                         ftp.cwd(config.file_location)
                         file_list = ftp.nlst()
-                        for filename in file_list :
-                            if filename not in processed_file_list:
+                        for filename in file_list:
+                            try:
                                 excel = io.BytesIO()
                                 ftp.retrbinary('RETR %s' % filename, excel.write)
                                 wb = xlrd.open_workbook(file_contents=excel.getvalue())
@@ -74,61 +67,56 @@ class FtpConfig(models.Model):
                                 header = sheet.row_values(0)
                                 header_ref = header.index('Order ID')
                                 header_fees = header.index('Total Fees')
-                                SaleOrder = self.env['sale.order']
                                 for i in range(1, sheet.nrows):
-                                    row = sheet.row_values(i)
-                                    order_id = row[header_ref]
-                                    if type(order_id) is float:
-                                        order_id = int(row[header_ref])
-                                    saleorder = SaleOrder.search([
-                                        ('client_order_ref', '=', str(order_id)),
-                                        ('state', 'not in', ['cancel']),
-                                        ('is_commission_updated', '=', False),
-                                    ], limit=1)
-                                    fee = row[header_fees]
-                                    if saleorder and fee != 0.0:
-                                        saleorder.write({
-                                            'total_fees': fee,
-                                            'is_commission_updated': True,
-                                        })
-                                        self.env.cr.commit()
+                                    try:
+                                        row = sheet.row_values(i)
+                                        fee = row[header_fees]
+                                        if fee != 0.0:
+                                            order_id = row[header_ref]
+                                            if type(order_id) is float:
+                                                order_id = int(row[header_ref])
+                                            saleorder = SaleOrder.search([
+                                                ('client_order_ref', '=', str(order_id)),
+                                                ('state', 'not in', ['cancel']),
+                                                ('is_commission_updated', '=', False),
+                                            ], limit=1)
+                                            if saleorder:
+                                                saleorder.write({
+                                                    'total_fees': fee,
+                                                    'is_commission_updated': True,
+                                                })
+                                                cr.commit()
+                                    except Exception as e:
+                                        cr.rollback()
+                                        logging.error(e)
 
-                                # config.move_file(ftp, config.dest_file_location, file)
+                                # Moving processed files
+                                self.move_file(ftp, filename, config.dest_file_location)
+
+                            except Exception as e:
+                                logging.error(e)
+
                 except Exception as e:
-                    self.env.cr.rollback()
                     logging.error(e)
-                    pass
+
                 finally:
                     if ftp:
-                        ftp.quit()
+                        ftp.close()
 
-        except Exception as e:
-            logging.error(e)
-            pass
+            elif config.protocol == 'sftp':
+                pass
+
         return True
 
-
     @api.model
-    def move_file(self, ftp, moveto_dir, file_tomove):
-        """
-        param@ ftp: ftp object
-        param@ moveto_dir: the dir to move(inside current dir)
-        param@ file_tomove: the file which is needed to move
-        """
-        r = BytesIO()
-        ftp.retrbinary('RETR %s'%(file_tomove), r.write)
-        file_data =  r.getvalue()
-        r.close()
-        ftp.cwd('/')
-        file_list = ftp.nlst()
-        if str(moveto_dir).replace('/','') not in file_list :
-            ftp.mkd(moveto_dir)
-        ftp.cwd(moveto_dir)
-        if file_tomove not in ftp.nlst():
-            data = io.BytesIO(file_data)
-            ftp.storbinary('STOR %s'%(file_tomove), data)
-            # ftp.cwd(config.file_location)
-            # ftp.delete(file_tomove)
+    def move_file(self, ftp, filename, destination):
+        try:
+            dest_file_path = '%s/%s' % (destination or '', filename)
+            if not dest_file_path.startswith('/'):
+                dest_file_path = '/' + dest_file_path
+            ftp.rename(filename, dest_file_path)
+        except Exception as e:
+            logging.error(e)
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
