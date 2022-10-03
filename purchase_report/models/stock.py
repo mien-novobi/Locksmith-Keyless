f3811c1f0e4d510156d043f8cace79093f5a082d
devika0712 <devika.bs@confianzit.biz>
2022-10-03 14:51:25 +0530

    name fixed

--- purchase_report/models/stock.py
+++ purchase_report/models/stock.py
@@ -9,7 +9,7 @@ from odoo import api, fields, models, _
 _logger = logging.getLogger(__name__)
 
 
-class StockPicking(models.Model):
+class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
     is_drop_shipping = fields.Boolean('Is dropship')
